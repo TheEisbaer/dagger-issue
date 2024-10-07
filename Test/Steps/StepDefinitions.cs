718943449707cf6d3ee18f92488291e8f64e5e6e
Dominik Niebuhr <dominik.niebuhr@goldbeck.de>
2024-10-07 12:22:59 +0200

    change back to minutes

--- Test/Steps/StepDefinitions.cs
+++ Test/Steps/StepDefinitions.cs
@@ -6,6 +6,6 @@ public sealed class StepDefinitions
     [Given(@"I wait for over (.*) minutes")]
     public async Task GivenIWaitForOverMinutes(int p0)
     {
-        await Task.Delay((int)TimeSpan.FromSeconds(p0+1).TotalMilliseconds);
+        await Task.Delay((int)TimeSpan.FromMinutes(p0+1).TotalMilliseconds);
     }
 }
